aa4b19f55eaa2ed1a63267eaa665200c48100df7
Destrifer <destrifer@yandex.ru>
2025-09-07 16:19:34 +0300

    fix

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -62,7 +62,7 @@ watch(open, (v) => {
         <img src="/logo.svg" alt="" class="h-10 w-10 mr-2" />
         <div class="flex leading-tight">
           <div
-            class="text-xs flex flex-col text-white items-center pr-4 border-r border-white/20"
+            class="hidden md:inline text-xs flex flex-col text-white items-center pr-4 border-r border-white/20"
           >
             <span>Сайт</span><span>психолога</span>
           </div>
